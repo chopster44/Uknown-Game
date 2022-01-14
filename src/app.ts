c668f0d343c0e1e2f1d70379cfd00b884967b467
chopster44 <oscargiacalone@hotmail.com>
2022-01-14 20:55:41 +0000

    first commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import "phaser";
+import { gameScene } from "./scenes/game";
+type GameConfig = Phaser.Types.Core.GameConfig
+const config: GameConfig = {
+    title: "Pong",
+    width: 800,
+    height: 600,
+    parent: "game",
+    scene: [gameScene],
+    backgroundColor: '#000000',
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    }
+};
+export class PongGame extends Phaser.Game {
+    constructor(config: GameConfig) {
+        super(config);
+    }
+};
+window.onload = () => {
+    var game = new PongGame(config);
+}
---
+++ src/scenes/game.ts
@@ -0,0 +1,19 @@
+import "phaser"; 
+export class gameScene extends Phaser.Scene {
+    constructor() {
+        super({
+            key: "Game"
+        });
+    }
+    
+    preload(): void {
+        //get all assets
+        
+    }
+    create(): void {
+       
+    }
+    update(time): void {
+        
+    }
+}
