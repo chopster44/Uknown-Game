393b7b98d8322c6d868f7def33deba00fb9231f8
chopster44 <oscargiacalone@hotmail.com>
2022-01-15 16:40:08 +0000

    added usefull stuf for player

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,5 +1,8 @@
 import "phaser"; 
 var player;
+var keyA, keyD, keySPACE;
+var Gravity: number = 50;
+var JumpDelay: number = 0;
 export class gameScene extends Phaser.Scene {
     constructor() {
         super({
@@ -8,13 +11,38 @@ export class gameScene extends Phaser.Scene {
     }
     
     preload(): void {
-        //get all assets
+        // load player sprite
         this.load.image('player', '../assets/SpriteA.png');
     }
     create(): void {
+        // display player sprite
         player = this.physics.add.sprite(50, 200, 'player');
+        
+        // player physics
+        player.setCollideWorldBounds(true);
+
+        // inputs
+        keyA = this.input.keyboard.addKey('A');
+        keyD = this.input.keyboard.addKey('D');
+        keySPACE = this.input.keyboard.addKey('SPACE');
     }
     update(time): void {
-        
+        if (keyA.isDown) {
+            player.setVelocity(-200, 0);
+        }
+        else if (keyD.isDown) {
+            player.setVelocity(200, 0);
+        }
+        else if (keySPACE.isDown && JumpDelay === 0) {
+            player.setVelocity(0, -400);
+            JumpDelay = 5;
+        }
+        else {
+            player.setVelocity(0, Gravity);
+        }
+
+        if (JumpDelay < 0) {
+            JumpDelay -= 1;
+        }
     }
 }
